8f3fc671f850ff8ab753388d61258ca9b5570e07
nueva-vaca <nuevavaca@hotmail.com>
2026-02-11 12:31:09 +0000

    -

--- JavaScript/bread.crum navigation.js
+++ JavaScript/bread.crum navigation.js
@@ -1,5 +1,5 @@
 function actualizarBreadcrumb() {
-// convertir la lógica del breadcrumb en una función y llamarla cada vez que se navegue a una nueva ruta
+// { convertir / englobar / wrap } la lógica del breadcrumb en una función y llamarla cada vez que se navegue a una nueva ruta
   
   
   
@@ -94,8 +94,42 @@ function actualizarBreadcrumb() {
 
 
 
-// Ejecutar al cargar la página
-actualizarBreadcrumb();
 
-// Exportar la función para usarla en otros archivos si es necesario
-window.actualizarBreadcrumb = actualizarBreadcrumb;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejecutar al cargar la página
+actualizarBreadcrumb()   ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Hacer la función 'actualizarBreadcrumb()' global ( para usarla en otros archivos (si es necesario) )
+window.actualizarBreadcrumb = actualizarBreadcrumb   ;
+// 'window' = 'objeto' que representa la ventana abierta del navegador
+// '.actualizarBreadcrumb' = 'una propiedad' del 'objeto' 'window' 
+// se le asigna la función 'actualizarBreadcrumb' -a: 'window.actualizarBreadcrumb' ---> y esto hace que : la función sea accesible globalmente, lo que permite que otros scripts en la página/ventana/navegador puedan llamarla/invocarla para actualizar el "breadcrumb" cuando sea necesario, como p.ej. después de una navegación o cambio de ruta.
